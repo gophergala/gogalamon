d92304a5a3f952b805c66fd56e004a33c4367baf
Derek Mayer <deverex1317@gmail.com>
2015-01-24 15:56:22 -0600

    Working on only removing objects we don't need to render anymore. And added in f key functionality

--- static/js/app.js
+++ static/js/app.js
@@ -43,58 +43,72 @@ function init() {
 	var listener = new window.keypress.Listener();
 
 	listener.register_many([
-	{
-	    "keys"       : "w",
-	    "on_keydown" : function() {
-            serverSock.send(JSON.stringify({
-				Event: "w down"
-			}));
-        },
-        "on_keyup"   : function(e) {
-            serverSock.send(JSON.stringify({
-				Event: "w up"
-			}));
-        }
-	},
-	{
-		"keys"       : "a",
-	    "on_keydown" : function() {
-            serverSock.send(JSON.stringify({
-				Event: "a down"
-			}));
-        },
-        "on_keyup"   : function(e) {
-            serverSock.send(JSON.stringify({
-				Event: "a up"
-			}));
-        }
-	},
-	{
-		"keys"       : "s",
-	    "on_keydown" : function() {
-            serverSock.send(JSON.stringify({
-				Event: "s down"
-			}));
-        },
-        "on_keyup"   : function(e) {
-            serverSock.send(JSON.stringify({
-				Event: "s up"
-			}));
-        }
-	},
-	{
-		"keys"       : "d",
-	    "on_keydown" : function() {
-            serverSock.send(JSON.stringify({
-				Event: "d down"
-			}));
-        },
-        "on_keyup"   : function(e) {
-            serverSock.send(JSON.stringify({
-				Event: "d up"
-			}));
-        }
-	}]);
+		{
+		    "keys"       : "w",
+		    "on_keydown" : function() {
+	            serverSock.send(JSON.stringify({
+					Event: "w down"
+				}));
+	        },
+	        "on_keyup"   : function(e) {
+	            serverSock.send(JSON.stringify({
+					Event: "w up"
+				}));
+	        }
+		},
+		{
+			"keys"       : "a",
+		    "on_keydown" : function() {
+	            serverSock.send(JSON.stringify({
+					Event: "a down"
+				}));
+	        },
+	        "on_keyup"   : function(e) {
+	            serverSock.send(JSON.stringify({
+					Event: "a up"
+				}));
+	        }
+		},
+		{
+			"keys"       : "s",
+		    "on_keydown" : function() {
+	            serverSock.send(JSON.stringify({
+					Event: "s down"
+				}));
+	        },
+	        "on_keyup"   : function(e) {
+	            serverSock.send(JSON.stringify({
+					Event: "s up"
+				}));
+	        }
+		},
+		{
+			"keys"       : "d",
+		    "on_keydown" : function() {
+	            serverSock.send(JSON.stringify({
+					Event: "d down"
+				}));
+	        },
+	        "on_keyup"   : function(e) {
+	            serverSock.send(JSON.stringify({
+					Event: "d up"
+				}));
+	    	}
+	    },
+	    {
+			"keys"       : "f",
+		    "on_keydown" : function() {
+	            serverSock.send(JSON.stringify({
+					Event: "f down"
+				}));
+	        },
+	        "on_keyup"   : function(e) {
+	            serverSock.send(JSON.stringify({
+					Event: "f up"
+				}));
+	    	}
+		}
+	]);
 
 
 	// Get the chat input box
@@ -146,20 +160,26 @@ function init() {
 	// Sweet jesus the normal prompts are ugly
 	var playerName = prompt("Please enter your player name");
 	serverSock.send(JSON.stringify({
-		Event: "username",
+		Event   : "username",
 		User    : playerName
 	}));
 
 
+	// Var to hold IDs of the backgrounds
+	var backgroundIds = [];
+
+
     function update() {
     	// To cache an object: DisplayObject.cache()
-    	// Remove all the objects on the canvas
-    	stage.removeAllChildren();
 
     	// Get the mainCanvas
     	var mainCanvas = document.getElementById("mainCanvas");
 
-    	
+
+
+    	removeOldChildren(stage.children, onScreenObjects);
+
+
 
 		// Place the far starfield
     	for (var i = mod(viewCenter.x * -0.1) - 512; i < mainCanvas.width; i += 512) {
@@ -168,42 +188,95 @@ function init() {
 				starFieldFar.x = i;
 				starFieldFar.y = j;
 
+				starFieldFar.name = backgroundIds.length;
+				onScreenObjects.push(starFieldFar);
+
 				stage.addChild(starFieldFar);
     		};
     	};
 
-    	// Place the near starfield
+    	// Place the mid starfield
     	for (var i = mod(viewCenter.x * -0.9) - 512; i < mainCanvas.width; i += 512) {
     		for (var j = mod(viewCenter.y * -0.9) - 512; j < mainCanvas.height; j += 512) {
     			var starFieldNear = new createjs.Bitmap("img/starfield_near.png");
 				starFieldNear.x = i;
 				starFieldNear.y = j;
 
+				starFieldNear.name = backgroundIds.length;
+				onScreenObjects.push(starFieldNear);
+
 				stage.addChild(starFieldNear);
     		};
     	};
 
+    	// Place the near starfield
+    	for (var i = mod(viewCenter.x * -0.9) - 512; i < mainCanvas.width; i += 512) {
+    		for (var j = mod(viewCenter.y * -0.9) - 512; j < mainCanvas.height; j += 512) {
+    			var starFieldMid = new createjs.Bitmap("img/starfield_middle.png");
+				starFieldMid.x = i;
+				starFieldMid.y = j;
+
+				starFieldMid.name = backgroundIds.length;
+				onScreenObjects.push(starFieldMid);
+
+				stage.addChild(starFieldMid);
+    		};
+    	};
+
+    	// Sort the IDs for the background
+    	backgroundIds.sort();
 
     	// Create and place each new object we're sent
     	for(var i = 0; i < onScreenObjects.length; i++) {
     		// Get the object we want to render
     		var currentObject = onScreenObjects[i];
-    		
-    		// Create the bitmap object
-    		var objectBitmap = new createjs.Bitmap("img/" + currentObject.N + ".png");
 
-    		objectBitmap.regX = 32;
-    		objectBitmap.regY = 32;
+    		var objectBitmap;
+    		var addChildBool;
+
+    		if(stage.children.indexOf(currentObject) == -1) {
+    			// Create the bitmap object
+    			objectBitmap = new createjs.Bitmap("img/" + currentObject.N + ".png");
+
+    			// Set the bitmap name to its unique id
+    			objectBitmap.name = currentObject.I;
+
+    			// Set the middle of the image (using the ship's size)
+	    		objectBitmap.regX = 32;
+	    		objectBitmap.regY = 32;
+
+	    		addChildBool = true;
+    		} else {
+    			objectBitmap = stage.children[stage.children.indexOf(currentObject)];
+
+    			addChildBool = false;
+    		} // end if/else
 
     		objectBitmap.x = Math.round(currentObject.X - viewCenter.x + mainCanvas.width/2);
     		objectBitmap.y = Math.round(currentObject.Y - viewCenter.y + mainCanvas.height/2);
     		objectBitmap.rotation = currentObject.R;
 
-    		stage.addChild(objectBitmap);
+    		// If the object is already on the stage, don't add it
+    		if(addChildBool) {
+    			stage.addChild(objectBitmap);
+    		} // end if
     	} // end for
+
+    	console.log(stage.numChildren);
 
 		stage.update();
 	} // end update()
+
+	function removeOldChildren(currentObjects, newObjects) {
+		for(var i = 0; i < currentObjects.length; i++) {
+			if(newObjects.indexOf(currentObjects[i]) == -1) {
+				var childToRemove = stage.getChildByName(currentObjects[i].I);
+
+				stage.removeChild(childToRemove);
+			} // end if
+		} // end for
+
+	} // end removeOldChildren()
 
 	function mod(z) {
 		z = z % 512;

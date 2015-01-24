c6abae36cdd6b162780c292aa3b9a07e493a62a2
Derek Mayer <deverex1317@gmail.com>
2015-01-23 21:05:25 -0600

    Connecting to a WebSocket to the server and sending specific key events.

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@ function init() {
 
 	var circle = new createjs.Shape();
 
-	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 25);
 	circle.x = 400;
 	circle.y = 250;
 	stage.addChild(circle);
@@ -18,19 +18,68 @@ function init() {
 
 	// Keypress listener
 	var listener = new window.keypress.Listener();
+	listener.register_many([
+	{
+	    "keys"       : "w",
+	    "on_keydown" : function() {
+            console.log("You pressed w");
+            serverSock.send(JSON.stringify({
+				Event: "w down"
+			}));
+        },
+        "on_keyup"   : function(e) {
+            console.log("And now you've released w.");
+            serverSock.send(JSON.stringify({
+				Event: "w up"
+			}));
+        }
+	},
+	{
+		"keys"       : "a",
+	    "on_keydown" : function() {
+            console.log("You pressed a");
+            serverSock.send(JSON.stringify({
+				Event: "a down"
+			}));
+        },
+        "on_keyup"   : function(e) {
+            console.log("And now you've released a.");
+            serverSock.send(JSON.stringify({
+				Event: "a up"
+			}));
+        }
+	},
+	{
+		"keys"       : "s",
+	    "on_keydown" : function() {
+            console.log("You pressed s");
+            serverSock.send(JSON.stringify({
+				Event: "s down"
+			}));
+        },
+        "on_keyup"   : function(e) {
+            console.log("And now you've released s.");
+            serverSock.send(JSON.stringify({
+				Event: "s up"
+			}));
+        }
+	},
+	{
+		"keys"       : "d",
+	    "on_keydown" : function() {
+            console.log("You pressed d");
+            serverSock.send(JSON.stringify({
+				Event: "d down"
+			}));
+        },
+        "on_keyup"   : function(e) {
+            console.log("And now you've released d.");
+            serverSock.send(JSON.stringify({
+				Event: "d up"
+			}));
+        }
+	}]);
 
-	listener.simple_combo("d", function() {
-		circle.x += moveStep;
-	});
-	listener.simple_combo("a", function() {
-		circle.x -= moveStep;
-	});
-	listener.simple_combo("w", function() {
-		circle.y -= moveStep;
-	});
-	listener.simple_combo("s", function() {
-		circle.y += moveStep;
-	});
 
 	// Get the chat input box
 	var chatInput = document.getElementById('chat_input');
@@ -50,6 +99,17 @@ function init() {
 	createjs.Ticker.setFPS(30);
 	//Update stage will render next frame
     createjs.Ticker.addEventListener("tick", update);
+
+
+
+
+    // Connect to the server's WebSocket
+    var serverSock = new WebSocket("ws://" + window.location.host + "/sock/");
+
+    serverSock.onmessage = function(str) {
+		console.log("Someone sent: ", str);
+	};
+
 
 
     function update() {
